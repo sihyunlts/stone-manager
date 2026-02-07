40b2bc52c0bd23b0ac029e4238881904e3f5cf7a
Si Hyun Baek <62822977+sihyunlts@users.noreply.github.com>
2026-02-08 03:24:31 +0900

    chore: add manual url to settings

--- src/settings.ts
+++ src/settings.ts
@@ -9,6 +9,11 @@ export function renderSettingsPage() {
       renderListItem({ label: "펌웨어 버전", valueId: "settingsFirmware" }),
       renderListItem({ label: "MAC 주소", valueId: "settingsMac" }),
       renderListItem({ label: "신호 강도 (RSSI)", valueId: "settingsRssi" }),
+      renderListItem({
+        label: "사용설명서",
+        clickable: true,
+        data: { url: "https://pfile.imholic.com:2822/Pantech/IM-100/IM-100S%20Manual%5B1%5D.pdf" },
+      }),
     ]),
   });
 
